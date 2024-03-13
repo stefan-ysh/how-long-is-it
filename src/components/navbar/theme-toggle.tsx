de65fe94ac46846c89a02070f38d13b3bfe19d39
yshuai <stefan_ysh@foxmail.com>
2024-03-13 23:56:25 +0800

    refactor(style): refactor function of changing theme

--- src/components/navbar/theme-toggle.tsx
+++ src/components/navbar/theme-toggle.tsx
@@ -7,13 +7,13 @@ import { Button } from '@/components/ui/button';
 
 export const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
-
+  const changeTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
   return (
     <Button
       variant="ghost"
       size="icon"
       aria-label="theme toggle"
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      onClick={changeTheme}
     >
       <Icons.sun className="dark:hidden" />
       <Icons.moon className="hidden dark:block" />
